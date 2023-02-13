6f364422e361e14a20b429df0413cebb9d2552f9
Fredy Diaz <Fredy.Diaz@vocusgroup.co.nz>
2023-02-14 06:51:16 +1300

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Find />
+</template>
+
+<script>
+import Find from './components/race/Find.vue';
+
+export default {
+  name: "App",
+  components: {
+    Find,
+  },
+  data() {
+    return {
+      title: 'My First Vue App'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/race/Find.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <label>Country</label>
+    <input type="text" />
+
+    <button @click="find">Find</button>
+  </div>
+  <div>
+    <div>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Country</th>
+        </thead>
+        <tbody>
+          <tr v-for="race in races">
+            <td>{{ race.name }}</td>
+            <td>{{ race.country }}</td>
+            <td>{{ race.region }}</td>
+            <td>{{ race.city }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      races: this.callFind(),
+    }
+  },
+  methods: {
+    find() {
+      this.races = this.callFind('a');
+    },
+    callFind(country) {
+      if (country == 'a') {
+        return [{ id: 3, name: 'Rigo' }, { id: 4, name: 'Gfny Colombia' }];
+      }
+
+      return [{ id: 1, name: 'Gfny Nyc' }, { id: 2, name: 'Gfny Dominican Republic' }];
+    }
+  }
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+}
+</style>
