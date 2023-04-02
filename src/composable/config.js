d9468147e386c3ca68003a47acbb645528efa9db
Fredy Diaz <Fredy.Diaz@vocusgroup.co.nz>
2023-04-03 06:42:31 +1200

    feat: test config vars

--- src/composable/config.js
+++ src/composable/config.js
@@ -1,6 +1,6 @@
 const config = {
   wero: {
-    url: process.env.APPSETTING_WERO__URL || 'http://localhost:7878/apis'
+    url: process.env.APPSETTING_WERO_URL || 'http://localhost:7878/apis'
   }
 };
 
