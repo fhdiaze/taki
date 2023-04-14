2d249268dcd72af160af9d58228ea622a7ac2a89
fredy.diaz <fredy.diaz@2degrees.nz>
2023-04-14 17:18:50 +1200

    feat: add load more

--- src/composable/ride.js
+++ src/composable/ride.js
@@ -2,25 +2,22 @@ import { config } from './config';
 
 /**
  * Gets rides that match a filter
- * @param {Object} filter The filters
- * @param {string} filter.name The name of the ride
- * @param {string} filter.description The description of the ride
- * @param {string} filter.city The city of the ride
- * @param {string} filter.country The country of the ride
+ * @param {object} cursor The query cursor
+ * @param {Object} cursor.query The filter query
+ * @param {string} cursor.query.name The name of the ride
+ * @param {string} cursor.query.description The description of the ride
+ * @param {string} cursor.query.city The city of the ride
+ * @param {string} cursor.query.country The country of the ride
  */
-const find = async (filter) => {
+const find = async (cursor) => {
   try {
-    const body = {
-      size: 10,
-      query: filter
-    };
     const options = {
       method: 'POST',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(cursor)
     };
     const response = await fetch(`${config.wero.url}/ride.find`, options);
     const page = await response.json();
