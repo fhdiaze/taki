005f00f73e8a6da45371a38f4b8ceeb8333d43f0
fredy.diaz <fredy.diaz@2degrees.nz>
2023-05-19 16:05:02 +1200

    feat: using typescript

--- src/components/NotFound.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section>
-    <h2>404</h2>
-    <h3>Page Not Found</h3>
-  </section>
-</template>
--- src/composable/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  wero: {
-    url: process.env.VUE_APP_WERO__URL || 'http://localhost:7878/api'
-  }
-};
-
-export { config };
