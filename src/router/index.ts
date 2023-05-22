039672ad18218c38581e5bd59baa8972b0ca346f
fredy.diaz <fredy.diaz@2degrees.nz>
2023-05-23 08:55:15 +1200

    fix: improve naming

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Find from '../views/ride/FindView.vue';
-import NotFound from '../components/NotFound.vue';
+import NotFound from '../views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
